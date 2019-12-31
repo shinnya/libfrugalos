a6e85665462d712ce48bdf7bb4b9de61955f2c32
Shinya Yamaoka <syamaoka@mail.libmacro.com>
2019-12-31 11:59:15 +0900

    fix todo

--- src/protobuf/consistency.rs
+++ src/protobuf/consistency.rs
@@ -2,9 +2,12 @@
 
 use protobuf_codec::field::branch::Branch4;
 use protobuf_codec::field::num::{F1, F2, F3, F4};
-use protobuf_codec::field::{FieldDecoder, FieldEncoder, Oneof};
+use protobuf_codec::field::{
+    FieldDecoder, FieldEncoder, MessageFieldDecoder, MessageFieldEncoder, Oneof,
+};
 use protobuf_codec::message::{MessageDecoder, MessageEncoder};
 use protobuf_codec::scalar::{Uint32Decoder, Uint32Encoder};
+use protobuf_codec::wellknown::google::protobuf::{EmptyMessageDecoder, EmptyMessageEncoder};
 
 use consistency::ReadConsistency;
 
@@ -13,9 +16,9 @@ use consistency::ReadConsistency;
 pub struct ReadConsistencyDecoder {
     inner: MessageDecoder<
         Oneof<(
-            FieldDecoder<F1, Uint32Decoder>,
-            FieldDecoder<F2, Uint32Decoder>,
-            FieldDecoder<F3, Uint32Decoder>,
+            MessageFieldDecoder<F1, EmptyMessageDecoder>,
+            MessageFieldDecoder<F2, EmptyMessageDecoder>,
+            MessageFieldDecoder<F3, EmptyMessageDecoder>,
             FieldDecoder<F4, Uint32Decoder>,
         )>,
     >,
@@ -34,9 +37,9 @@ impl_message_decode!(ReadConsistencyDecoder, ReadConsistency, |t: _| {
 pub struct ReadConsistencyEncoder {
     inner: MessageEncoder<
         Oneof<(
-            FieldEncoder<F1, Uint32Encoder>,
-            FieldEncoder<F2, Uint32Encoder>,
-            FieldEncoder<F3, Uint32Encoder>,
+            MessageFieldEncoder<F1, EmptyMessageEncoder>,
+            MessageFieldEncoder<F2, EmptyMessageEncoder>,
+            MessageFieldEncoder<F3, EmptyMessageEncoder>,
             FieldEncoder<F4, Uint32Encoder>,
         )>,
     >,
@@ -45,11 +48,10 @@ impl_sized_message_encode!(
     ReadConsistencyEncoder,
     ReadConsistency,
     |item: Self::Item| {
-        // TODO enum に対応する値が必要ない場合どうするか？を検討
         match item {
-            ReadConsistency::Consistent => Branch4::A(0),
-            ReadConsistency::Stale => Branch4::B(0),
-            ReadConsistency::Quorum => Branch4::C(0),
+            ReadConsistency::Consistent => Branch4::A(()),
+            ReadConsistency::Stale => Branch4::B(()),
+            ReadConsistency::Quorum => Branch4::C(()),
             ReadConsistency::Subset(n) => Branch4::D(n as u32),
         }
     }
